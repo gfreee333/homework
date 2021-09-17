9697ad55a91362a2391fe06bfb396dfadeae9c3b
ЧухмановИван <oollvanolloo@yandex.ru>
2021-09-17 23:07:16 +0400

    практическое задание номер 1.2 создание конструкторов.

--- src/test/java/Person.java
+++ src/test/java/Person.java
@@ -1,3 +1,5 @@
+package test.java;
+
 public class Person {
     private String FirstName;
     private String LastName;
@@ -6,25 +8,31 @@ public class Person {
 
     }
     public String getFirstName() {
+
         return FirstName;
     }
     public String getLastName() {
+
         return LastName;
     }
     public int getPassportId()
     {
+
         return PassportId;
     }
     public void setFirstName(String FirstName)
     {
+
         this.FirstName=FirstName;
     }
     public void setLastName(String LastName)
     {
+
         this.LastName = LastName;
     }
     public void setPassportId(int PassportId)
     {
+
         this.PassportId = PassportId;
     }
     public static void main (String[] args)
