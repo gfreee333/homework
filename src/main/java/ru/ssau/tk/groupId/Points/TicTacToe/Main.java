dd6a1b0a266d1e4dc49085507ce6ccc8fc34ab63
ЧухмановИван <oollvanolloo@yandex.ru>
2021-09-23 21:38:05 +0400

    создаю игру крестики нолики начало созание экрана ( добавлены шаблоны функций ) создан коснтрустктор и функция запуска программы.

---
+++ src/main/java/ru/ssau/tk/groupId/Points/TicTacToe/Main.java
@@ -0,0 +1,8 @@
+package ru.ssau.tk.groupId.Points.TicTacToe;
+
+public class Main {
+    public static void main (String [] args)
+    {
+        TikTacToe tikTacToe = new TikTacToe();
+    }
+}
---
+++ src/main/java/ru/ssau/tk/groupId/Points/TicTacToe/TikTacToe.java
@@ -0,0 +1,47 @@
+package ru.ssau.tk.groupId.Points.TicTacToe;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+public class TikTacToe implements ActionListener {
+    Random random = new Random();
+    JFrame frame = new JFrame();
+    JPanel title_panel = new JPanel();
+    JPanel button_panel = new JPanel();
+    JLabel textfield = new JLabel();
+    JButton[] buttons = new JButton[9];
+    boolean player1_turn;
+
+    TikTacToe() {
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800,800);
+        frame.getContentPane().setBackground(Color.black);
+        frame.setLayout(new BorderLayout());
+        frame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    public void firstTurn() {
+
+    }
+
+    public void check() {
+
+    }
+
+    public void xWins(int a, int b, int c) {
+
+    }
+
+    public void oWins(int a, int b, int c) {
+
+    }
+
+}
