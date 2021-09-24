85bf181af760dcd8633515908f45b6e333104552
ЧухмановИван <oollvanolloo@yandex.ru>
2021-09-24 12:24:29 +0400

    расширил конструктор.

--- src/main/java/ru/ssau/tk/groupId/Points/TicTacToe/TikTacToe.java
+++ src/main/java/ru/ssau/tk/groupId/Points/TicTacToe/TikTacToe.java
@@ -18,9 +18,19 @@ public class TikTacToe implements ActionListener {
     TikTacToe() {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800,800);
-        frame.getContentPane().setBackground(Color.black);
+        frame.getContentPane().setBackground(new Color(50,50,50));
         frame.setLayout(new BorderLayout());
         frame.setVisible(true);
+        textfield.setBackground(new Color(25,25,25));
+        textfield.setForeground(new Color(25,225,0));
+        textfield.setFont(new Font("Ink free",Font.BOLD,75));
+        textfield.setHorizontalAlignment(JLabel.CENTER);
+        textfield.setText("TicTacToe");
+        textfield.setOpaque(true);
+        title_panel.setLayout(new BorderLayout());
+        title_panel.setBounds(0,0,800,100);
+        title_panel.add(textfield);
+        frame.add(title_panel);
     }
 
     @Override
