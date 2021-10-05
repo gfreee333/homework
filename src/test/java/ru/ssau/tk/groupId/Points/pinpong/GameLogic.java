74a7b500b5428b5471a748e5162f1d9e0750e462
Чухманов Иван Викторович <oollvanolloo@yandex.ru>
2021-10-06 03:33:30 +0400

    Добавил отрисовку мячика и ракеток.

--- src/test/java/ru/ssau/tk/groupId/Points/pinpong/GameLogic.java
+++ src/test/java/ru/ssau/tk/groupId/Points/pinpong/GameLogic.java
@@ -1,6 +1,7 @@
 package ru.ssau.tk.groupId.Points.pinpong;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -8,10 +9,24 @@ import java.awt.event.KeyListener;
 
 public class GameLogic extends JPanel implements ActionListener, KeyListener {
 
+    private final int Bit1Y = 160;
+    private final int Bit2Y = 160;
+    private final int BallX = 350;
+    private  final int BallY = 150;
+    GameLogic() {
+        addKeyListener(this);
+    }
 
-
-
-
+    @Override
+    public void paint(Graphics g) {
+        g.setColor(Color.black);
+        g.fillRect(0,0,800,500);
+        g.setColor(Color.white);
+        g.fillRect(0,Bit1Y,10,100);
+        g.setColor(Color.white);
+        g.fillRect(770,Bit2Y,10,100);
+        g.fillRect(BallX,Bit1Y,20,20);
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
