f20bd5b129574b5889e710fe74d56769608ef508
Чухманов Иван Викторович <oollvanolloo@yandex.ru>
2021-10-06 00:15:01 +0400

    задание 3.2

--- src/test/java/ru/ssau/tk/groupId/Points/string/SomeString.java
+++ src/test/java/ru/ssau/tk/groupId/Points/string/SomeString.java
@@ -1,5 +1,7 @@
 package ru.ssau.tk.groupId.Points.string;
 
+import java.nio.charset.StandardCharsets;
+
 public class SomeString {
 
     public void lineOutput(String string) {
@@ -8,10 +10,25 @@ public class SomeString {
             System.out.println(string.charAt(i));
         }
     }
+    public void getStringBytes(String myString)
+    {
+        byte[] stringBytes = myString.getBytes();
+        for (int bytes : stringBytes)
+        {
+            System.out.println(bytes);
+        }
+    }
+
 
     public static void main(String[] args) {
+        System.out.println("task 3.1");
         String s1 = "HELLO WORLD";
+        String s2 = "привет мир";
         SomeString String = new SomeString();
         String.lineOutput(s1);
+        System.out.println("new task 3.2: ");
+        String.getStringBytes(s1);
+        String.getStringBytes(s2);
     }
+
 }
