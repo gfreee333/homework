5a32e18a24e38484cf17bbe33aa872a6f1b3bed2
Чухманов Иван Викторович <oollvanolloo@yandex.ru>
2021-10-12 01:57:17 +0400

    смог написать в чате ботом hello,world <3

--- src/main/java/ru/ssau/tk/groupId/Points/telegrambot/TestBot.java
+++ src/main/java/ru/ssau/tk/groupId/Points/telegrambot/TestBot.java
@@ -3,20 +3,29 @@ package ru.ssau.tk.groupId.Points.telegrambot;
 import lombok.SneakyThrows;
 import org.telegram.telegrambots.bots.DefaultAbsSender;
 import org.telegram.telegrambots.bots.DefaultBotOptions;
+import org.telegram.telegrambots.meta.api.methods.send.SendAnimation;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class TestBot extends DefaultAbsSender {
-    public TestBot(DefaultBotOptions options){
+    public TestBot(DefaultBotOptions options) {
         super(options);
     }
+
     @Override
-    public String getBotToken(){
+    public String getBotToken() {
         return "2094032540:AAF2TIbb_8F18jMx7q9Ivuuopek1W9B-R84";
     }
+
     @SneakyThrows
-    public static void main(String [] args)
-    {
+    public static void main(String[] args) {
         TestBot bot = new TestBot(new DefaultBotOptions());
-        bot.execute(SendMessage.builder().chatId("1082432833").text("hello,world").build());
+        try {
+            bot.execute(SendMessage.builder().chatId("1082432833").text("hello, world <3").build());
+        }
+        catch (TelegramApiException e){
+            e.printStackTrace();
+        }
     }
 }
